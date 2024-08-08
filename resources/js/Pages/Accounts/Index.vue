e7fe66596dc927f0bea810402854221cd69bdf9c
Molodoy3 <artem.nikonov.2005@bk.ru>
2024-08-08 17:58:21 +0500

    f

--- resources/js/Pages/Accounts/Index.vue
+++ resources/js/Pages/Accounts/Index.vue
@@ -111,42 +111,6 @@ onUnmounted(() => {
 
         <Head title="Аккаунты"></Head>
         <section class="accounts">
-            <!--            <ais-instant-search id="search-wrap" :search-client="client" index-name="accounts" class='accounts__container'>
-                            <div class="accounts__header">
-                                <Title>Все аккаунты</Title>
-                                <ais-search-box placeholder="Поиск аккаунтов..."></ais-search-box>
-                            </div>
-                            <a :href="route('accounts.create')" class="accounts__add button">Добавить</a><br>
-                            <div v-if="$page.props.flash.message" class="message">
-                                <button  class="button-delete-message">X</button>
-                                {{$page.props.flash.message}}
-                            </div>
-                            <ais-hits>
-                                    <template v-slot:item="{ item }">
-                                        <div class="accounts__item" :class="{ 'accounts__item_busy': item.busy, 'accounts__item_process': item.status == 1, 'accounts__item_problem': item.status == 2}">
-                                            <h3 class="accounts__title-item">{{ item.games }}</h3>
-                                            <div class="accounts__info">
-                                                <div class="accounts__info-title">Логин:</div> <span class="accounts__weight">{{
-                                                    item.login }}</span>
-                                            </div>
-                                            <div class="accounts__info">
-                                                <div class="accounts__info-title">Пароль:</div> <span class="accounts__weight">{{
-                                                    item.password }}</span>
-                                            </div>
-                                            <div class="accounts__status">
-                                                <div class="accounts__free" v-if="item.busy == null && item.status == null">Свободен</div>
-                                                <div class="accounts__process" v-if="item.status == 1">В процессе публикации</div>
-                                                <div class="accounts__problem" v-if="item.status == 2">Проблема(ы)</div>
-                                                <div class="accounts__date" v-if="item.busy && item.status == null">{{ formatDate(item.busy) }}</div>
-                                            </div>
-                                            <div class="accounts__buttons">
-                                                <Button @click="router.get('/accounts/' + item.id + '/edit')">Перейти</Button>
-                                                <Button @click="copy('Логин: ' + item.login + ' \\n Пароль: ' + item.password)" class="accounts__copy">Копировать</Button>
-                                            </div>
-                                        </div>
-                                    </template>
-</ais-hits>
-</ais-instant-search>-->
             <div class='accounts__container'>
                 <div class="accounts__header">
                     <Title>Все аккаунты</Title>
